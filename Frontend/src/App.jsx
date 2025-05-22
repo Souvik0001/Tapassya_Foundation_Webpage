1b244df7dcc0f4c7ef9ccf2a646af69ee351fb94
Souvik0001 <souvikchowdhury095@gmail.com>
2025-05-22 13:57:38 +0530

    Initial Commit ...

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar.jsx'
+import Home from './pages/Home/Home.jsx'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar/>
+      <Home/>
+      
+    </div>
+  )
+}
+
+export default App
